cfb4a0a9c35f6d9e27f918950060e84c547d9b37
rikvanderwerf <rikvanderwerf@invyte.app>
2020-04-16 07:25:50 +0200

    stash:

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('gaphql');
 
 const app = express();
 
 app.use(express.json())
 
-app.get('/', (req, res, next) => {
-	res.send('Hello World');
+app.get('/graphql', graphqlHttp({
+    schema: buildSchema(`
+	schema {
+	    query:  
+	    mutation:
+	}
+    `),
+    rootValue: {
+	
+    }
+
 })
 
 app.listen(3000)
