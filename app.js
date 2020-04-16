378f1c4096d22e703774177976fd39253cd3e3b8
rikvanderwerf <rikvanderwerf@invyte.app>
2020-04-16 09:30:18 +0200

    setup uuid

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql');
+
 const { buildSchema } = require('graphql');
+const { v4 } = require('uuid');
+
+const uuid = v4
 
 const app = express();
+const retailers = [];
 
 app.use(express.json())
 
 var schema = buildSchema(`
+	type Retailer {
+		id: ID!
+		name: String!
+	}
+
+	input RetailerInput {
+		name: String!
+	}
+
 	type rootQuery {
-		retailers: [String!]!	
+		retailers: [Retailer!]!	
 	}
 
 	type rootMutation {
-		createRetailer(name: String): String
+		createRetailer(retailerInput: RetailerInput): Retailer
 	}
 	
 	schema {
@@ -26,6 +40,10 @@ var rootResolver = {
 		return ['skafshop']; 
 	},
 	createRetailer: (args) => {
+		const retailer = {
+			id: uuid(),
+			name: args.title
+		}
 		const retailerName = args.name;
 		return retailerName
 	}
