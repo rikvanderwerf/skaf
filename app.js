855c865d89cf612d25627fdf7b10c2e907b37758
rikvanderwerf <rikvanderwerf@invyte.app>
2020-04-18 11:22:58 +0200

    added linting

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql');
 
 const { sequelize } = require('./database/database.js');
-const retailer = require('./models/retailer.js');
 const schema = require('./schemas/schema.js');
 
 const app = express();
