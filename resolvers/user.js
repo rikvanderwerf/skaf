21b0790df49da65dc4ec34f65a00835f72555942
rikvanderwerf <rikvanderwerf@invyte.app>
2020-04-18 13:18:34 +0200

    encrypt password

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,12 +1,20 @@
+const bcrypt = require('bcryptjs')
 const user = require('../models/user.js')
 const { handleDatabaseQueryPromise } = require('./resolver.js')
 
 const userResolver = {
 	rootMutation: {
 		createUser: (_, args) => {
-			return handleDatabaseQueryPromise(
-				user.create(args.userInput)
-			)
+			bcrypt.hash(args.userInput.password, 12)
+				.then(password => {
+					args.userInput.password = password
+					return handleDatabaseQueryPromise(
+						user.create(args.userInput)
+					)
+				})
+				.catch(err => {
+					throw err
+				})
 		}
 	}
 }
