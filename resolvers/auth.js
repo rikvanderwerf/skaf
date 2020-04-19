bb47301c75fd29fc96442e0ed36a34870f25c110
rikvanderwerf <rikvanderwerf@invyte.app>
2020-04-19 15:05:26 +0200

    improved readability

--- resolvers/auth.js
+++ resolvers/auth.js
@@ -1,34 +1,33 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const User = require('../models/user.js')
-const { handleDatabaseQueryPromise } = require('./resolver.js')
+
+async function login(_, args) {
+	try {
+		const user = await User.findUser({
+			"email": args.authInput.email
+		}) 
+		const passwordIsCorrect = await bcrypt.compare(args.authInput.password, user.password)
+		if (!passwordIsCorrect) {
+			throw new Error('Invalid credentials')
+		}
+		const token = jwt.sign(
+			{userId: user.id, email: user.email},
+			'privateKey',
+			{ expiresIn: '1h'}
+		)
+		return {
+			userId: user.id, 
+			token: token,
+		}
+	} catch(error) {
+		throw err
+	}
+}
 
 const authResolver = {
 	rootQuery: {
-		login: (_, args) => {
-			return User.findUser({
-				"email": args.authInput.email
-			})
-			.then(user => {
-				return bcrypt.compare(args.authInput.password, user.password)
-				.then(isPasswordCorrect => {
-					if (isPasswordCorrect) {
-						const token = jwt.sign({userId: user.id, email: user.email}, 'privateKey', {
-							expiresIn: '1h'
-						})
-						return {
-							userId: user.id, 
-							token: token,
-						}
-					} else {
-						throw new Error("Invalid credentials")
-					}
-				})	
-			})
-			.catch(err => {
-				throw err 
-			})
-		}
+		login: login 	
 	}
 }
 
