4369040d8320b52a459d5f68cd85cdeb13ba596f
rikvanderwerf <rikvanderwerf@invyte.app>
2020-04-22 15:10:07 +0200

    delete dist

--- dist/database/database.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-exports.sequelize = new sequelize_1.Sequelize('postgres://login_role:password@postgresql_db:5432/postgres');
-//# sourceMappingURL=database.js.map
--- dist/lib/errors.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UnAuthenticatedError = new Error('Unauthenticated');
-//# sourceMappingURL=errors.js.map
